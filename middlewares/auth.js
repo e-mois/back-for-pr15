eb44f42139e5339893654381d0ddbcbd00ec39b9
Eugene <eugen.moiseev@gmail.com>
2022-09-12 00:39:50 +0300

    Upload files for server

---
+++ middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const NotAuthError = require('../errors/NotAuthError');
+
+const auth = (req, res, next) => {
+  const token = req.cookies.access_token;
+  console.log(req.cookies);
+
+  if (!token) {
+    next(new NotAuthError('Необходима авторизация'));
+  }
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'secret-code');
+  } catch (err) {
+    next(new NotAuthError('Необходима авторизация'));
+    return;
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  console.log(req.user);
+
+  next(); // пропускаем запрос дальше
+};
+
+module.exports = { auth };
